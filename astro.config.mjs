a1fda7d368a8d9c78fbbe7c521d374b6a544c3ee
Andrés <afernandez@viviendea.com>
2023-07-23 20:16:33 +0200

    vercel configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-	experimental: {
-		viewTransitions: true,
-	},
-	site: "https://andresfera99.github.io",
-	base: "/astro-katana",
+  experimental: {
+    viewTransitions: true
+  },
+  site: "https://andresfera99.github.io",
+  base: "/astro-katana",
+  output: "server",
+  adapter: vercel()
 });
